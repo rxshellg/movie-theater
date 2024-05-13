21913ec2686c9955f0b72f556447dc43fe1a5b43
rxshellg <rashellguerrero123@gmail.com>
2024-05-13 06:34:45 -0400

    Created Express Server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Define port for Express
+app.listen(PORT, () => {
+    console.log(`Now live on port ${PORT}`)
+})
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
